2c6df42174cc924b05b2e00ef1d4e280f0cd92f5
danielstrolle <danielstrolle@gmail.com>
2017-10-16 10:48:03 -0500

    Complete the Deployment and Dependencies exercise, Importing StringUtils from apache to be able to take userInput and swapCase, reverse, and check if its a number

--- src/main/java/com/codeup/App.java
+++ src/main/java/com/codeup/App.java
@@ -1,6 +1,8 @@
 package com.codeup;
 
 import org.apache.commons.lang3.StringUtils;
+import java.util.Scanner;
+
 
 /**
  * Hello world!
@@ -10,7 +12,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        boolean isBlank = StringUtils.isBlank("      ");
-        System.out.println(isBlank);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter Something: ");
+        String userInput = scan.nextLine();
+        System.out.println("You Entered: '" + userInput + "'");
+        if (StringUtils.isNumeric(userInput)) {
+            System.out.println(userInput + " is a number");
+        } else {
+            System.out.println(userInput + " is not a number");
+        }
+        String swap = StringUtils.swapCase(userInput);
+        System.out.println("Flipped Case: " + swap);
+        String reverse = StringUtils.reverse(userInput);
+        System.out.println("Reversed: " + reverse);
     }
 }
