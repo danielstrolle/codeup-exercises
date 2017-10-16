b8926b6c27cf2fab35829bf0385c00585ecbd18b
danielstrolle <danielstrolle@gmail.com>
2017-10-16 09:58:22 -0500

    Initial commit

---
+++ src/main/java/com/codeup/App.java
@@ -0,0 +1,16 @@
+package com.codeup;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        boolean isBlank = StringUtils.isBlank("      ");
+        System.out.println(isBlank);
+    }
+}
